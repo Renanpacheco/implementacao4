25e21bf6b6425360ab66990a9fa0c41cd30668c3
Renan <prenan86@gmail.com>
2018-11-16 20:21:26 -0200

    delta 1 feito

--- implementacao4.py
+++ implementacao4.py
@@ -15,7 +15,7 @@ nIBGE = len(xIBGE)
 xEmbriao = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 yEmbriao = [0.029, 0.052, 0.079, 0.125, 0.181, 0.261, 0.425, 0.738, 1.130, 1.882, 2.812]
 nEmbriao = len(xEmbriao)
-xvar=[0.029]
+#xvar=[0.029]
 #print(xEmbriao[0])
 #Questao 1:
 #a)
@@ -25,7 +25,7 @@ plt.title('Dados Funcao 1')
 plt.show()
 '''
 #b
-def funcao1():
+#print(yEmbriao[0])
 	
 
 #c
@@ -44,8 +44,24 @@ plt.show()
 '''
 
 #b
+d1=[]
+'''
+d1=(yEmbriao[1]-yEmbriao[0])/(xEmbriao[1]-xEmbriao[0])
+d1=(yEmbriao[2]-yEmbriao[1])/(xEmbriao[2]-xEmbriao[1])
+d1=(yEmbriao[2]-yEmbriao[1])/(xEmbriao[2]-xEmbriao[1])
+'''
+#i=0
 
+aux=0.0
 
+for i in range(nEmbriao-1): 
+	#i=i+1
+	aux=(yEmbriao[i+1]-yEmbriao[i])/(xEmbriao[i+1]-xEmbriao[i])
+	#i=i+1
+	
+	d1.append(aux)
+
+print(d1)
 
 #c
 
