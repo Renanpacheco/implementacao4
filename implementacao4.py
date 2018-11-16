19288c4adda2bce45b4d1e5609cef98100f76085
Renanpacheco <prenan86@gmail.com>
2018-11-16 20:26:01 -0200

    delta 1 feito

--- implementacao4.py
+++ implementacao4.py
@@ -7,40 +7,65 @@ from numpy import linalg
 import matplotlib.pyplot as plt
 
 #Tabela Questao 1:
-xIBGE = np.array([1872, 1890, 1900, 1920, 1940, 1950, 1960, 1970, 1980, 1991, 1996], dtype = 'double')
-yIBGE = np.array([9.9, 14.3, 17.4, 30.6, 41.2, 51.9, 70.2, 93.1, 119.0, 146.2, 157.1], dtype = 'double')
+xIBGE = [1872, 1890, 1900, 1920, 1940, 1950, 1960, 1970, 1980, 1991, 1996]
+yIBGE = [9.9, 14.3, 17.4, 30.6, 41.2, 51.9, 70.2, 93.1, 119.0, 146.2, 157.1]
 nIBGE = len(xIBGE)
 
-#Tabela Questao 2:
-xEmbriao = np.array([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], dtype = 'double')
-yEmbriao = np.array([0.029, 0.052, 0.079, 0.125, 0.181, 0.261, 0.425, 0.738, 1.130, 1.882, 2.812], dtype = 'double')
+
+xEmbriao = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+yEmbriao = [0.029, 0.052, 0.079, 0.125, 0.181, 0.261, 0.425, 0.738, 1.130, 1.882, 2.812]
 nEmbriao = len(xEmbriao)
-
-
+#xvar=[0.029]
+#print(xEmbriao[0])
 #Questao 1:
 #a)
+'''
 plt.plot(xIBGE,yIBGE,'ro')
 plt.title('Dados Funcao 1')
 plt.show()
-
+'''
 #b
-
+#print(yEmbriao[0])
+	
 
 #c
 
-#d
+#dGIT 
 
 #e
 
 #Questao 2:
 
 #a
+'''
 plt.plot(xEmbriao,yEmbriao,'ro')
 plt.title('Dados Funcao 2')
 plt.show()
+'''
 
 #b
+d1=[]
+'''
+d1=(yEmbriao[1]-yEmbriao[0])/(xEmbriao[1]-xEmbriao[0])
+d1=(yEmbriao[2]-yEmbriao[1])/(xEmbriao[2]-xEmbriao[1])
+d1=(yEmbriao[2]-yEmbriao[1])/(xEmbriao[2]-xEmbriao[1])
+'''
+#i=0
+
+aux=0.0
+
+for i in range(nEmbriao-1): 
+	#i=i+1
+	aux=(yEmbriao[i+1]-yEmbriao[i])/(xEmbriao[i+1]-xEmbriao[i])
+	#i=i+1
+	
+	d1.append(aux)
+
+print(d1)
 
 #c
 
 #d
+
+
+ 
